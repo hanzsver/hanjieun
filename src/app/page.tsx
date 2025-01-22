f190d4d1c4d53e960848f1bff8393488a779202c
hanzsver <hanzsver@gmail.com>
2025-01-22 15:13:58 +0900

    feat: pretendard, styled-components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,83 @@
-import Image from "next/image";
+"use client";
+
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import { pxToRem } from "../utils/pxToRem";
+
+// 동적 스타일
+const Limit = styled.div<{ size: "s" | "m" | "l" | "xl" }>`
+  width: 100%;
+  margin: 0 auto;
+  max-width: ${({ size }) => `var(--limit-${size})`};
+`;
+
+const spacingMap = {
+  s: pxToRem(20),
+  m: pxToRem(40),
+  l: pxToRem(80),
+};
+
+const Spacing = styled.div<{ size: keyof typeof spacingMap }>`
+  margin-top: ${({ size }) => spacingMap[size]};
+  margin-bottom: ${({ size }) => spacingMap[size]};
+`;
+
+// 개별 스타일
+const Container = styled.div`
+  background-color: ${({ theme }) => theme.colors.background};
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+const ContactBar = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  background-color: var(--color-primary);
+  color: var(--color-white);
+`;
+
+const SubContent = styled.div`
+  margin: 135px 0;
+`;
+
+const Title = styled.h2`
+  font-size: ${pxToRem(44)};
+  font-weight: 600;
+`;
+
+const SubTitle = styled.h3`
+  font-size: ${pxToRem(24)};
+  font-weight: 600;
+`;
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <Container>
+      <ContactBar>이거슨 팝업</ContactBar>
+      <Limit as="main" size="xl">
+        <SubContent>
+          <Limit size="m">
+            {/* Title */}
+            <Spacing size="l">
+              <Spacing size="m">
+                <Title>
+                  대타이틀.
+                  <br />
+                  대타이틀 대타이틀 대타이틀대타이틀.
+                </Title>
+              </Spacing>
+              <SubTitle>
+                소타이틀 소타이틀 소타이틀,
+                소타이틀소타이틀소타이틀소타이틀소타이틀소타이틀소타이틀소타이틀.
+              </SubTitle>
+            </Spacing>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            {/* Skills */}
+            <Spacing size="l">
+              <SubTitle>소타이틀2</SubTitle>
+            </Spacing>
+          </Limit>
+        </SubContent>
+      </Limit>
+    </Container>
   );
 }
